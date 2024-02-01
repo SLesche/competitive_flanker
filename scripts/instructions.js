59763129901499743a37ce2040e50054ee05fe6b
Sven Lesche <113923517+SLesche@users.noreply.github.com>
2024-02-01 09:57:18 +0100

    init split screen

--- scripts/instructions.js
+++ scripts/instructions.js
@@ -4,3 +4,17 @@ const instructions = {
 }
   
 timeline.push(instructions)
+
+const split_upper_half = {
+    type: jsPsychHtmlKeyboardResponse,
+    stimulus: '<div class="upper-center">Upper Half</div>', 
+}
+
+timeline.push(split_upper_half)
+
+const split_lower_half = {
+    type: jsPsychHtmlKeyboardResponse,
+    stimulus: '<div class="lower-center">Lower Half</div>', 
+}
+
+timeline.push(split_lower_half)
