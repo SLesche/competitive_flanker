42f8ee5fc5dbf701c5b759b95a1e75cc4726d92c
Sven Lesche <113923517+SLesche@users.noreply.github.com>
2024-02-11 16:38:34 +0100

    better instructions

--- scripts/debriefing.js
+++ scripts/debriefing.js
@@ -1,7 +1,7 @@
 // here debriefing
 const debriefing = {
     type: jsPsychHtmlKeyboardResponse,
-    stimulus: 'Vielen Dank für deine Teilnahme!',
+    stimulus: '<p class = "normal-text">Vielen Dank für deine Teilnahme!</p>',
     data: {type: 'instructions'},
     on_start: function() {
       jsPsych.data.get().localSave('csv', experiment_file);
