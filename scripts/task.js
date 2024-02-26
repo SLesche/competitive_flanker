7df149905e9d5a3ca82e75215e230b87703b960f
Sven Lesche <113923517+SLesche@users.noreply.github.com>
2024-02-26 10:19:18 +0100

    right order

--- scripts/task.js
+++ scripts/task.js
@@ -7,9 +7,14 @@ function map_responses_to_letters(subject_number, letter_combinations) {
       // Determine responses based on even or odd subject_number
       const response1 = subject_number % 2 === 0 ? 'd' : 'l';
       const response2 = subject_number % 2 === 0 ? 'l' : 'd';
-  
-      response_mapping[char1] = response1;
-      response_mapping[char2] = response2;
+      
+      if (response1 == 'd'){
+        response_mapping[char1] = response1;
+        response_mapping[char2] = response2;
+      } else {
+        response_mapping[char2] = response2;
+        response_mapping[char1] = response1;
+      }
     }
   
     return response_mapping;
