85658e127116d166c2a58b563da9e9c4b87f0bba
Sven Lesche <113923517+SLesche@users.noreply.github.com>
2024-02-27 09:58:36 +0100

    score 0 and instructions

--- scripts/instructions.js
+++ scripts/instructions.js
@@ -54,6 +54,25 @@ const survey_trial = {
     data: {type: 'demographics'},
 };
 
+var instructions = {
+    type: jsPsychInstructions,
+    pages: [
+     '<div class = "normal-text">Du wirst im Laufe des Experimentes eine Aufgabe entweder alleine, mit einer/m Partner:in oder gegen einer/n Partner:in bearbeiten.</br>Dabei kannst du für jede richtige Antwort Punkte sammeln. Wenn du am Ende unter den besten 25% bist, hast du automatisch die Chance einen Gutschein zu gewinnen.</br>Spielst du alleine, zählen nur deine Punkte in dieser Runde. Spielst du <b>mit</b> einer/m Partner:in, zählen auch richtige Antworten deiner Partnerin. Spielst du <b>gegen</b> eine/n Partner:in, so werden richtige Antworten des Partners oder der Partnerin am Ende von deinem Score abgezogen.</div>',
+     '<div class = "normal-text">Da wir euch nicht zu zweit an den PC setzen wollen und nicht garantieren können, dass immer zwei Personen da sind, werden eure Antworten aufgezeichnet und als Partner:in für die nächste Person, die an eurem Rechner sitzt, benutzt.</br>Ihr spielt also nicht "live" mit/gegen eine Partner:in, sondern mit der letzten Versuchsperson.</br>Die Antwort auf der oberen Hälfte des Screens ist immer die Antwort des Partners oder der Partnerin (rot), die untere Hälfte ist eure (grün).</div>',
+     '<div class = "normal-text">Deine Aufgabe ist es, in einer Reihe von 5 Buchstaben auf den mittleren dieser Buchstaben zu reagieren. </br> Bevor die Aufgabe losgeht, wird dir immer gesagt, mit welcher Taste ("D" oder "L") du auf welchen Buchstaben reagieren musst. </br> Wenn z.B. A -> "D" und B -> "L", dann musst du bei "AA<b>A</b>AA" oder "BB<b>A</b>BB" mit "D" reagieren.</br>Bei AA<b>B</b>AA oder BB<b>B</b>BB müsstest du mit "L" reagieren.</div>',
+     `<div class = "normal-text">Für jeden Durchgang wird dir am Anfang angezeigt, auf welchen Buchstaben in der Mitte du wie zu reagieren hast.</br>Dann hast du ${n_practice} Durchgänge Zeit, um einmal ohne Partner:in und ohne Score zu üben, bevor es dann richtig losgeht.</div>`,
+
+    ],
+    show_clickable_nav: true, 
+    data: {type: 'instructions'},
+    on_finish: function(data){
+        // after instructions, the conditions start
+        current_condition = conditions[0];
+        icondition = 1;
+    },
+    button_label_next: "Weiter",
+    button_label_previous: "Zurück",
+}
 const consent_screen = {
     type: jsPsychHtmlButtonResponse,
     stimulus: '<div class = "normal-text">Hier Consent</div>',
@@ -86,6 +105,8 @@ const procedure_instructions = {
     data: {type: 'instructions'},
 }
 
+
+/*
 const instructions = {
     timeline: [general_description, partner_description, flanker_description, procedure_instructions],
     on_finish: function(data){
@@ -94,3 +115,4 @@ const instructions = {
         icondition = 1;
     }
 }
+*/
