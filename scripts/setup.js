6eaf459316591865059e04e41c061921d25a30c9
Sven Lesche <113923517+SLesche@users.noreply.github.com>
2024-04-29 17:35:39 +0200

    new instruction images

--- scripts/setup.js
+++ scripts/setup.js
@@ -21,9 +21,9 @@ let timeline = [];
 
 // IN the real experiment, 300 responses.
 // 3*100 pro Block
-const n_trials = 1; // 100
-const n_blocks = 1; // 3
-const n_practice = 1; // 30
+const n_trials = 100; // 100
+const n_blocks = 3; // 3
+const n_practice = 30; // 30
 const n_letter_combinations = 1;
 const possible_response_keys = ["d", "l"];
 const possible_stimuli = ["MN", "EF", "OQ", "VU", "IT", "PR"];
