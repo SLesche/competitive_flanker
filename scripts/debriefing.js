a6344232d8aae13fdc88300be0bfdf7f239267bb
Sven Lesche <113923517+SLesche@users.noreply.github.com>
2024-05-08 14:25:36 +0200

    id and call for experimenter

--- scripts/debriefing.js
+++ scripts/debriefing.js
@@ -2,7 +2,7 @@
 let debriefing = {
     type: jsPsychHtmlKeyboardResponse,
     stimulus: function(data) {
-      return `<p class = "normal-text">Vielen Dank für deine Teilnahme!</br>Du hast ${total_score} Punkte erreicht. Dieser Teil der Studie ist jetzt beendet.</p>`
+      return `<p class = "normal-text">Vielen Dank für deine Teilnahme!</br>Du hast ${total_score} Punkte erreicht. Dieser Teil der Studie ist jetzt beendet.</p><br><p class = "normal-text"><b>Suche kurz nach der Versuchsleiterin / dem Versuchsleiter. Dann startet der letzte Teil der Studie.</b></p>`
     },
     data: {type: 'instructions'},
     on_start: function() {
